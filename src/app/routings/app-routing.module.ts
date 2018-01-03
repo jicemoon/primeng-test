9e7a7282e7fff133d7db2de1e64ee105a3b3ada1
jicemoon <jicemoon@outlook.com>
2018-01-03 14:41:30 +0800

    add data/carousel test

--- src/app/routings/app-routing.module.ts
+++ src/app/routings/app-routing.module.ts
@@ -5,19 +5,26 @@ import { MenuItem, SelectButton } from 'primeng/primeng';
 
 import * as btnConfigs from './button-routing.config';
 import * as inputConfigs from './input-routing.config';
+import * as dataConfigs from './data-routing.config';
 
 export const routeComponents = [
   ...btnConfigs.components,
-  ...inputConfigs.components
+  ...inputConfigs.components,
+  ...dataConfigs.components
 ];
 const routes: Routes = [{
-  path: '',
-  redirectTo: '/button/btn',
-  pathMatch: 'full'
-}, btnConfigs.routeConfig, inputConfigs.routeConfig];
+    path: '',
+    redirectTo: '/button/btn',
+    pathMatch: 'full'
+  },
+  btnConfigs.routeConfig,
+  inputConfigs.routeConfig,
+  dataConfigs.routeConfig
+];
 export const menuItems: MenuItem[] = [
   btnConfigs.menuItem,
-  inputConfigs.menuItem
+  inputConfigs.menuItem,
+  dataConfigs.menuItem
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/routings/data-routing.config.ts
@@ -0,0 +1,35 @@
+import { Route } from '@angular/router';
+import { MenuItem, SelectButton } from 'primeng/primeng';
+
+import { CarouselComponent } from './../components/pages/data/carousel/carousel.component';
+
+export const components = [
+  CarouselComponent
+];
+
+export const routeConfig: Route = {
+  path: 'data',
+  children: [
+    {
+      path: '',
+      redirectTo: 'carousel',
+      pathMatch: 'prefix'
+    },
+    {
+      path: 'carousel',
+      component: CarouselComponent
+    }
+  ]
+};
+
+export const menuItem: MenuItem = {
+  label: '数据显示',
+  icon: 'fa-bus',
+  items: [
+    {
+      label: 'Carousel',
+      routerLink: '/data/carousel'
+    }
+  ]
+};
+
